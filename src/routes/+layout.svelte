d32eac23a9be6d0ffe4c4386d8b72e8a80a770bd
Fran <mildmoose@pm.me>
2023-06-06 11:21:03 -0600

    Added Vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
     import "../app.css";
-    import Nav from "../lib/components/nav.svelte";
+    import Nav from "$lib/components/nav.svelte";
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="pt-4 h-screen flex flex-col justify-between">
